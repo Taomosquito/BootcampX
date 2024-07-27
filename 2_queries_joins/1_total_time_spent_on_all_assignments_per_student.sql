b265203ba55f62c6073203525e4dd4998f791d87
Taomosquito <S_Peloquin123@live.com>
2024-07-26 23:54:23 -0400

    revise format

--- 2_queries_joins/1_total_time_spent_on_all_assignments_per_student.sql
+++ 2_queries_joins/1_total_time_spent_on_all_assignments_per_student.sql
@@ -1,3 +1,4 @@
-SELECT SUM("duration") FROM "students"
+SELECT SUM("duration") AS total_duration
+FROM "students"
 JOIN "assignment_submissions" ON "students"."id" = "student_id"
 WHERE "name" = 'Ibrahim Schimmel';
--- 2_queries_joins/2_total_time_spent_on_all_assignments_per_cohort.sql
+++ 2_queries_joins/2_total_time_spent_on_all_assignments_per_cohort.sql
@@ -1,4 +1,5 @@
-SELECT SUM("duration") FROM "students"
+SELECT SUM("duration") AS total_duration
+FROM "students"
 JOIN "assignment_submissions" ON "students"."id" = "student_id"
 JOIN "cohorts" ON "cohorts"."id" = "cohort_id" 
 WHERE "cohorts"."name" = 'FEB12';
